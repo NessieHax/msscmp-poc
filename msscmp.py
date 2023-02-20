743d5f2a0a9b89f385349163fed4c1e6f6c64ba4
miku-666 <74728189+NessieHax@users.noreply.github.com>
2023-02-20 18:21:44 +0100

    Update class interface of msscmp class

--- msscmp.py
+++ msscmp.py
@@ -32,8 +32,11 @@ class Logger:
             return caller
         return decorater
 
-    def log(__obj: object, logTarget: str | None = ...) -> None:
-        logFilename = f"{logTarget}.txt" if isinstance(logTarget, str) else f"{__obj}.txt"
+    def logIf(condition: bool, __obj: object, targetFile: str | None = ...) -> None:
+        if (condition): Logger.log(__obj, targetFile)
+
+    def log(__obj: object, targetFile: str | None = ...) -> None:
+        logFilename = f"{targetFile}.txt" if isinstance(targetFile, str) else f"{__obj}.txt"
         with open(logFilename, "a") as logFile:
             print(f"[{datetime.datetime.time(datetime.datetime.utcnow())}]: {pprint.pformat(__obj)}", file=logFile)
 
@@ -46,13 +49,13 @@ class BufferedDataReader:
     def stream(self) -> BufferedReader:
         return self._stream
 
-    def readInts(self, count: int) -> tuple[int]:
+    def readInts(self, count: int) -> tuple[int, ...]:
         return struct.unpack(f"{self._endianness}{count}i", self._stream.read(count*4))
 
     def readInt(self) -> int:
         return self.readInts(1)[0]
 
-    def readFloats(self, count: int) -> tuple[float]:
+    def readFloats(self, count: int) -> tuple[float, ...]:
         return struct.unpack(f"{self._endianness}{count}f", self._stream.read(count*4))
 
     def readFloat(self) -> float:
@@ -62,7 +65,7 @@ class BufferedDataReader:
         return self.readStringAt(self._stream.tell())
 
     def readStringAt(self, offset: int) -> str:
-        return readAt(self._stream, offset, self.readUntil, b'\x00').decode("UTF-8")
+        return readAt(self._stream, offset, self.readUntil, b'\x00').decode("ASCII")
 
     def readUntil(self, stopper: bytes) -> bytes:
         result = bytearray()
@@ -76,7 +79,7 @@ def readAt(stream: BufferedReader, offset: int, func: Callable[[Ellipsis], Any],
     stream.seek(origin, SEEK_SET)
     return result
 
-@dataclass(slots=True)
+@dataclass(frozen=True, slots=True)
 class BankHeader:
     name: str
     filename: str
@@ -85,7 +88,7 @@ class BankHeader:
 
 @dataclass(frozen=True, slots=True)
 class BankSource:
-    bankPathOffset: int
+    path_offset: int
     path: str
     file_name: str
     file_size: int
@@ -93,6 +96,7 @@ class BankSource:
     data_offset: int
     play_action: int
     unknown_data: dict = field(default_factory=dict)
+    data: bytes = field(default_factory=bytes, repr=False)
 
 @dataclass(slots=True)
 class BankEvent:
@@ -111,7 +115,7 @@ class MsscmpParser:
     bankinfo: BankInfo = field(init=False, default_factory=BankInfo)
     verbose: bool = field(default=False)
 
-    def process(self, stream: BufferedReader, dump_path: str = None):
+    def process(self, stream: BufferedReader):
         #! 0x42414e4b == b'BANK' | Big Endian
         #! 0x4b4e4142 == b'KNAB' | Little Endian
         signature = stream.read(4)
@@ -121,9 +125,10 @@ class MsscmpParser:
 
         self.bankinfo.header = self.readBankHeader(reader)
 
-        _, event_table_start, source_table_start, *_  = reader.readInts(5) # zero, event_offset, source_table_start, ?, ?
-        data_end, event_count = reader.readInts(2)
-        *_, source_count = reader.readInts(3)
+        print(f"{stream.tell()=}")
+
+        __start,       event_table_start, *__unknown_data_table_start, source_table_start  = reader.readInts(5)
+        __start_count, event_count,       *__unknown_data_count,       source_count        = reader.readInts(5)
 
         if (stream.tell() != event_table_start):
             stream.seek(event_table_start, SEEK_SET)
@@ -134,9 +139,11 @@ class MsscmpParser:
             raw_property_string = reader.readStringAt(offset_event_details)
             event = BankEvent(event_name, raw_property_string)
 
+            Logger.logIf(self.verbose, event, "MSSEvents")
+
             event.properties = self.decodeEventProperties(raw_property_string)
 
-            self.bankinfo.events[event_name] = event
+            self.bankinfo.events[os.path.dirname(event_name)] = event
 
         if (stream.tell() != source_table_start):
             stream.seek(source_table_start, SEEK_SET)
@@ -144,50 +151,52 @@ class MsscmpParser:
         for _ in range(source_count):
             path_offset, info_offset = reader.readInts(2)
             source: BankSource = readAt(stream, info_offset, self.readBankSource, reader, path_offset)
-            if self.bankinfo.events.get(os.path.dirname(source.path), None) is not None:
-                self.bankinfo.events[os.path.dirname(source.path)].sources.append(source)
+            event_name: str = os.path.dirname(source.path)
+            if self.bankinfo.events.get(event_name, None) is not None:
+                self.bankinfo.events[event_name].sources.append(source)
 
-            Logger.log(source, "BankSources")
+            Logger.logIf(self.verbose, source, "BankSources")
 
-            if dump_path is not None:
-                self.dumpSource(stream, dump_path, source)
+    def dumpAllSources(self, path: str) -> None:
+        for event in self.bankinfo.events.values():
+            for source in event.sources:
+                self.dumpSource(source, path)
 
-    def dumpSource(self, stream: BufferedReader, dump_path: str, bankSource: BankSource):
-        path = f"{dump_path}/{os.path.dirname(bankSource.path)}"
-        if not os.path.exists(path): os.makedirs(path)
-        with open(f"{dump_path}/{bankSource.path}.binka", "wb") as f:
-            binka_data = readAt(stream, bankSource.data_offset, stream.read, bankSource.file_size)
-            f.write(binka_data)
+    def dumpSource(self, bankSource: BankSource, path: str):
+        full_path = f"{path}/{os.path.dirname(bankSource.path)}"
+        if not os.path.exists(full_path): os.makedirs(full_path)
+        with open(f"{path}/{bankSource.path}.binka", "wb") as f:
+            f.write(bankSource.data)
 
     def decodeEventProperties(self, raw_property_event_string: str) -> list[str]:
         properties: list[str] = raw_property_event_string.split(';')
         return properties
 
     def readBankHeader(self, reader: BufferedDataReader) -> BankHeader:
-        # version: Has to be 8 to match runtime
-        # mem_usage: Number of bytes required to build entry data(name, file_name, file_location, file_size, ...)
+        # @version: Has to be 8 to match runtime
+        # @mem_usage: Number of bytes required to build source info(name, file_name, file_location, file_size, ...)
         version, mem_usage, _ = reader.readInts(3)
         filename = reader.readString()
         name = reader.readStringAt(0x38)
         return BankHeader(name, filename, mem_usage, version)
 
-    def readBankSource(self, reader: BufferedDataReader, source_name_offset: int) -> BankSource:
+    def readBankSource(self, reader: BufferedDataReader, source_path_offset: int) -> BankSource:
         current_offset = reader.stream.tell()
         unknown_data = dict()
         unknown_data["source_offset"] = current_offset
 
         recived_source_name_offset = reader.readInt()
-        if (source_name_offset != recived_source_name_offset):
-            raise Exception(f"Unexpected offset difference: Expected {source_name_offset}(0x{source_name_offset:x}) got {recived_source_name_offset}(0x{recived_source_name_offset:x})")
+        if (source_path_offset != recived_source_name_offset):
+            raise Exception(f"Unexpected offset difference: Expected {source_path_offset}(0x{source_path_offset:x}) got {recived_source_name_offset}(0x{recived_source_name_offset:x})")
 
-        bankPath = reader.readStringAt(source_name_offset)
+        path_name = reader.readStringAt(source_path_offset)
+        # The filename is build up of the file size and the file offset written as decimal number separated by an asterisks('*') and end inf the '.binka' file extention.
         file_name = reader.readStringAt(reader.readInt() + current_offset)
         unknown_data["0x08"] = hex(reader.readInt())
-        play_action = reader.readInt() # type(1 = play, 2 = loop) note: not tested!
+        play_action = reader.readInt() # type(1 = play, 2 = loop) note: random guess based on seen source objcets
         unknown_data["0x10"] = reader.readInt()
         sample_rate = reader.readInt()
         # Minimal file buffer size allocated/alignment: 4Kb
-        # Note: not tested with larger binka files
         file_size = reader.readInt()
         unknown_data["Channels"] = reader.readInt()
         unknown_data["0x20"] = reader.readInt()
@@ -196,8 +205,14 @@ class MsscmpParser:
         unknown_data["0x28"] = reader.readInt()
         unknown_data["0x2C"] = reader.readInt()
         unknown_data["0x30"] = reader.readInt()
-        unknown_data["0x34"] = reader.readFloat() # distant scalar
+        unknown_data["0x34"] = reader.readFloat()
         unknown_data["0x38"] = reader.readInt()
+        
+        if unknown_data["0x2C"] or unknown_data["0x30"] or unknown_data["0x38"] > 0:
+            print(unknown_data)
+
         data_offset = int(file_name.split('*')[-1].rsplit('.')[0]) # yes :^)
 
-        return BankSource(source_name_offset, bankPath, file_name, file_size, sample_rate, data_offset, play_action, unknown_data)
+        data = readAt(reader.stream, data_offset, reader.stream.read, file_size)
+
+        return BankSource(source_path_offset, path_name, file_name, file_size, sample_rate, data_offset, play_action, unknown_data, data)
--- parse.py
+++ parse.py
@@ -7,13 +7,18 @@ def main():
     argparser.add_argument("-v", "--verbose", action="store_true", default=False, help="Output debug info (be verbose).")
     argparser.add_argument("-d", "--dump", action="store", default=None, metavar="path", help="Dumps bank sources into a directory.")
     args = argparser.parse_args()
+
+    print(args)
     
     if not args.filepath.endswith(".msscmp"):
         raise Exception("Not a Soundbank(.msscmp) file")
 
-    with open(args.filepath, "rb") as file:
+    with open(args.filepath, "rb") as file: 
         msscmpfile = msscmp.MsscmpParser(args.verbose)
-        msscmpfile.process(file, args.dump)
+        msscmpfile.process(file)
+        if args.dump is not None:
+            print("Dumping...")
+            msscmpfile.dumpAllSources(args.dump)
 
 if __name__ == "__main__":
     main()
